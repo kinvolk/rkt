5fc5b62dce7a33506cb35b3d6835158e84318e3c
Iago López Galeiras <iago@kinvolk.io>
2016-02-24 18:42:09 +0100

    Godeps: bump update go-systemd

--- Godeps/_workspace/src/github.com/coreos/go-systemd/util/util.go
+++ Godeps/_workspace/src/github.com/coreos/go-systemd/util/util.go
@@ -18,9 +18,7 @@
 // than linking against them.
 package util
 
-// #cgo LDFLAGS: -ldl
 // #include <stdlib.h>
-// #include <dlfcn.h>
 // #include <sys/types.h>
 // #include <unistd.h>
 //
@@ -58,54 +56,24 @@ package util
 // }
 import "C"
 import (
-	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 	"syscall"
 	"unsafe"
+
+	"github.com/coreos/pkg/dlopen"
 )
 
-var ErrSoNotFound = errors.New("unable to open a handle to libsystemd")
+var libsystemdNames = []string{
+	// systemd < 209
+	"libsystemd-login.so.0",
+	"libsystemd-login.so",
 
-// libHandle represents an open handle to the systemd C library
-type libHandle struct {
-	handle  unsafe.Pointer
-	libname string
-}
-
-func (h *libHandle) Close() error {
-	if r := C.dlclose(h.handle); r != 0 {
-		return fmt.Errorf("error closing %v: %d", h.libname, r)
-	}
-	return nil
-}
-
-// getHandle tries to get a handle to a systemd library (.so), attempting to
-// access it by several different names and returning the first that is
-// successfully opened. Callers are responsible for closing the handler.
-// If no library can be successfully opened, an error is returned.
-func getHandle() (*libHandle, error) {
-	for _, name := range []string{
-		// systemd < 209
-		"libsystemd-login.so",
-		"libsystemd-login.so.0",
-
-		// systemd >= 209 merged libsystemd-login into libsystemd proper
-		"libsystemd.so",
-		"libsystemd.so.0",
-	} {
-		libname := C.CString(name)
-		defer C.free(unsafe.Pointer(libname))
-		handle := C.dlopen(libname, C.RTLD_LAZY)
-		if handle != nil {
-			h := &libHandle{
-				handle:  handle,
-				libname: name,
-			}
-			return h, nil
-		}
-	}
-	return nil, ErrSoNotFound
+	// systemd >= 209 merged libsystemd-login into libsystemd proper
+	"libsystemd.so.0",
+	"libsystemd.so",
 }
 
 // GetRunningSlice attempts to retrieve the name of the systemd slice in which
@@ -113,8 +81,8 @@ func getHandle() (*libHandle, error) {
 // This function is a wrapper around the libsystemd C library; if it cannot be
 // opened, an error is returned.
 func GetRunningSlice() (slice string, err error) {
-	var h *libHandle
-	h, err = getHandle()
+	var h *dlopen.LibHandle
+	h, err = dlopen.GetHandle(libsystemdNames)
 	if err != nil {
 		return
 	}
@@ -124,11 +92,8 @@ func GetRunningSlice() (slice string, err error) {
 		}
 	}()
 
-	sym := C.CString("sd_pid_get_slice")
-	defer C.free(unsafe.Pointer(sym))
-	sd_pid_get_slice := C.dlsym(h.handle, sym)
-	if sd_pid_get_slice == nil {
-		err = fmt.Errorf("error resolving sd_pid_get_slice function")
+	sd_pid_get_slice, err := h.GetSymbolPointer("sd_pid_get_slice")
+	if err != nil {
 		return
 	}
 
@@ -162,8 +127,8 @@ func GetRunningSlice() (slice string, err error) {
 // unable to successfully open a handle to the library for any reason (e.g. it
 // cannot be found), an errr will be returned
 func RunningFromSystemService() (ret bool, err error) {
-	var h *libHandle
-	h, err = getHandle()
+	var h *dlopen.LibHandle
+	h, err = dlopen.GetHandle(libsystemdNames)
 	if err != nil {
 		return
 	}
@@ -173,11 +138,8 @@ func RunningFromSystemService() (ret bool, err error) {
 		}
 	}()
 
-	sym := C.CString("sd_pid_get_owner_uid")
-	defer C.free(unsafe.Pointer(sym))
-	sd_pid_get_owner_uid := C.dlsym(h.handle, sym)
-	if sd_pid_get_owner_uid == nil {
-		err = fmt.Errorf("error resolving sd_pid_get_owner_uid function")
+	sd_pid_get_owner_uid, err := h.GetSymbolPointer("sd_pid_get_owner_uid")
+	if err != nil {
 		return
 	}
 
@@ -210,8 +172,8 @@ func RunningFromSystemService() (ret bool, err error) {
 // `sd_pid_get_unit` call, with the same caveat: for processes not part of a
 // systemd system unit, this function will return an error.
 func CurrentUnitName() (unit string, err error) {
-	var h *libHandle
-	h, err = getHandle()
+	var h *dlopen.LibHandle
+	h, err = dlopen.GetHandle(libsystemdNames)
 	if err != nil {
 		return
 	}
@@ -221,11 +183,8 @@ func CurrentUnitName() (unit string, err error) {
 		}
 	}()
 
-	sym := C.CString("sd_pid_get_unit")
-	defer C.free(unsafe.Pointer(sym))
-	sd_pid_get_unit := C.dlsym(h.handle, sym)
-	if sd_pid_get_unit == nil {
-		err = fmt.Errorf("error resolving sd_pid_get_unit function")
+	sd_pid_get_unit, err := h.GetSymbolPointer("sd_pid_get_unit")
+	if err != nil {
 		return
 	}
 
@@ -244,7 +203,7 @@ func CurrentUnitName() (unit string, err error) {
 }
 
 // IsRunningSystemd checks whether the host was booted with systemd as its init
-// system. This functions similar to systemd's `sd_booted(3)`: internally, it
+// system. This functions similarly to systemd's `sd_booted(3)`: internally, it
 // checks whether /run/systemd/system/ exists and is a directory.
 // http://www.freedesktop.org/software/systemd/man/sd_booted.html
 func IsRunningSystemd() bool {
@@ -253,4 +212,16 @@ func IsRunningSystemd() bool {
 		return false
 	}
 	return fi.IsDir()
+}
+
+// GetMachineID returns a host's 128-bit machine ID as a string. This functions
+// similarly to systemd's `sd_id128_get_machine`: internally, it simply reads
+// the contents of /etc/machine-id
+// http://www.freedesktop.org/software/systemd/man/sd_id128_get_machine.html
+func GetMachineID() (string, error) {
+	machineID, err := ioutil.ReadFile("/etc/machine-id")
+	if err != nil {
+		return "", fmt.Errorf("failed to read /etc/machine-id: %v", err)
+	}
+	return strings.TrimSpace(string(machineID)), nil
 }
