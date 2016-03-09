3edc4f2024fc8d3e2ca20c56ea6d49f3b816fcb0
Krzesimir Nowak <krzesimir@kinvolk.io>
2016-03-09 12:34:47 +0100

    functional tests: Strip unnecessary newlines

--- tests/rkt_trust_test.go
+++ tests/rkt_trust_test.go
@@ -37,49 +37,49 @@ func TestTrust(t *testing.T) {
 	ctx := testutils.NewRktRunCtx()
 	defer ctx.Cleanup()
 
-	t.Logf("Run the non-signed image: it should fail\n")
+	t.Logf("Run the unsigned image: it should fail")
 	runImage(t, ctx, imageFile, "error opening signature file", true)
 
-	t.Logf("Sign the images\n")
+	t.Logf("Sign the images")
 	ascFile := runSignImage(t, imageFile, 1)
 	defer os.Remove(ascFile)
 	ascFile = runSignImage(t, imageFile2, 1)
 	defer os.Remove(ascFile)
 
-	t.Logf("Run the signed image without trusting the key: it should fail\n")
+	t.Logf("Run the signed image without trusting the key: it should fail")
 	runImage(t, ctx, imageFile, "openpgp: signature made by unknown entity", true)
 
-	t.Logf("Trust the key with the wrong prefix\n")
+	t.Logf("Trust the key with the wrong prefix")
 	runRktTrust(t, ctx, "wrong-prefix.com/my-app", 1)
 
-	t.Logf("Run a signed image with the key installed in the wrong prefix: it should fail\n")
+	t.Logf("Run a signed image with the key installed in the wrong prefix: it should fail")
 	runImage(t, ctx, imageFile, "openpgp: signature made by unknown entity", true)
 
-	t.Logf("Trust the key with the correct prefix, but wrong key\n")
+	t.Logf("Trust the key with the correct prefix, but wrong key")
 	runRktTrust(t, ctx, "rkt-prefix.com/my-app", 2)
 
-	t.Logf("Run a signed image with the wrong key installed: it should fail\n")
+	t.Logf("Run a signed image with the wrong key installed: it should fail")
 	runImage(t, ctx, imageFile, "openpgp: signature made by unknown entity", true)
 
-	t.Logf("Trust the key with the correct prefix\n")
+	t.Logf("Trust the key with the correct prefix")
 	runRktTrust(t, ctx, "rkt-prefix.com/my-app", 1)
 
-	t.Logf("Finally, run successfully the signed image\n")
+	t.Logf("Finally, run successfully the signed image")
 	runImage(t, ctx, imageFile, "Hello", false)
 	runImage(t, ctx, imageFile2, "openpgp: signature made by unknown entity", true)
 
-	t.Logf("Trust the key on unrelated prefixes\n")
+	t.Logf("Trust the key on unrelated prefixes")
 	runRktTrust(t, ctx, "foo.com", 1)
 	runRktTrust(t, ctx, "example.com/my-app", 1)
 
-	t.Logf("But still only the first image can be executed\n")
+	t.Logf("But still only the first image can be executed")
 	runImage(t, ctx, imageFile, "Hello", false)
 	runImage(t, ctx, imageFile2, "openpgp: signature made by unknown entity", true)
 
-	t.Logf("Trust the key for all images (rkt trust --root)\n")
+	t.Logf("Trust the key for all images (rkt trust --root)")
 	runRktTrust(t, ctx, "", 1)
 
-	t.Logf("Now both images can be executed\n")
+	t.Logf("Now both images can be executed")
 	runImage(t, ctx, imageFile, "Hello", false)
 	runImage(t, ctx, imageFile2, "Hello", false)
 }
